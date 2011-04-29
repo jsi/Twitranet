154d2f037d94abb1120105e932a205820ae3b38d
Jørgen Sivesind <jsi@enonic.com>
2011-04-29 16:50:28 +0200

    Definitions of menus on the login and main activity.

--- gen/com/enonic/android/twitranet/R.java
+++ gen/com/enonic/android/twitranet/R.java
@@ -21,53 +21,68 @@ public final class R {
         public static final int icon=0x7f020007;
         public static final int icon_images=0x7f020008;
         public static final int icon_images_folder=0x7f020009;
-        public static final int preferences_icon=0x7f02000a;
-        public static final int th_line=0x7f02000b;
-        public static final int user_bhj_64=0x7f02000c;
-        public static final int user_daa_64=0x7f02000d;
-        public static final int user_dummy_64=0x7f02000e;
-        public static final int user_jsi_64=0x7f02000f;
-        public static final int user_jvs_64=0x7f020010;
-        public static final int user_liv_64=0x7f020011;
-        public static final int user_ljl=0x7f020012;
-        public static final int user_mer_64=0x7f020013;
-        public static final int user_rmy_64=0x7f020014;
-        public static final int user_tan=0x7f020015;
-        public static final int user_tlo_64=0x7f020016;
-        public static final int user_tre_64=0x7f020017;
-        public static final int user_tsi_64=0x7f020018;
+        public static final int logout_icon=0x7f02000a;
+        public static final int preferences_icon=0x7f02000b;
+        public static final int th_line=0x7f02000c;
+        public static final int user_bhj_64=0x7f02000d;
+        public static final int user_daa_64=0x7f02000e;
+        public static final int user_dummy_64=0x7f02000f;
+        public static final int user_jsi_64=0x7f020010;
+        public static final int user_jvs_64=0x7f020011;
+        public static final int user_liv_64=0x7f020012;
+        public static final int user_ljl=0x7f020013;
+        public static final int user_mer_64=0x7f020014;
+        public static final int user_rmy_64=0x7f020015;
+        public static final int user_tan=0x7f020016;
+        public static final int user_tlo_64=0x7f020017;
+        public static final int user_tre_64=0x7f020018;
+        public static final int user_tsi_64=0x7f020019;
     }
     public static final class id {
-        public static final int clearLoginButton=0x7f050007;
-        public static final int dateText=0x7f05000f;
-        public static final int emptyLine=0x7f050000;
-        public static final int entryItem=0x7f05000c;
-        public static final int entryText=0x7f05000e;
-        public static final int loginButton=0x7f050006;
-        public static final int loginPrompt=0x7f050001;
-        public static final int newMessage=0x7f050009;
-        public static final int newMessageLabel=0x7f050008;
-        public static final int password=0x7f050005;
-        public static final int passwordPrompt=0x7f050004;
-        public static final int send=0x7f05000a;
-        public static final int slett=0x7f05000b;
-        public static final int userImage=0x7f05000d;
-        public static final int username=0x7f050003;
-        public static final int usernamePrompt=0x7f050002;
+        public static final int clearLoginButton=0x7f060007;
+        public static final int dateText=0x7f06000f;
+        public static final int emptyLine=0x7f060000;
+        public static final int entryItem=0x7f06000c;
+        public static final int entryText=0x7f06000e;
+        public static final int loginButton=0x7f060006;
+        public static final int loginPrompt=0x7f060001;
+        public static final int logout_menu=0x7f060011;
+        public static final int newMessage=0x7f060009;
+        public static final int newMessageLabel=0x7f060008;
+        public static final int password=0x7f060005;
+        public static final int passwordPrompt=0x7f060004;
+        public static final int preferences_menu=0x7f060010;
+        public static final int send=0x7f06000a;
+        public static final int slett=0x7f06000b;
+        public static final int userImage=0x7f06000d;
+        public static final int username=0x7f060003;
+        public static final int usernamePrompt=0x7f060002;
     }
     public static final class layout {
         public static final int login_layout=0x7f030000;
         public static final int twitranett_layout=0x7f030001;
         public static final int twitranett_list_item=0x7f030002;
     }
+    public static final class menu {
+        public static final int login_menu=0x7f050000;
+        public static final int main_menu=0x7f050001;
+    }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int clear_button_en=0x7f04000f;
         public static final int clear_button_no=0x7f04000e;
+        public static final int key_password=0x7f040015;
+        /**  internal keys 
+         */
+        public static final int key_username=0x7f040014;
         public static final int login_button_en=0x7f04000d;
         public static final int login_button_no=0x7f04000c;
         public static final int login_promt_en=0x7f040007;
         public static final int login_promt_no=0x7f040006;
+        public static final int menu_logout_en=0x7f040012;
+        public static final int menu_logout_no=0x7f040013;
+        public static final int menu_preferences_en=0x7f040010;
+        public static final int menu_preferences_no=0x7f040011;
         public static final int password_prompt_en=0x7f04000b;
         public static final int password_prompt_no=0x7f04000a;
         public static final int prompt_en=0x7f040005;
